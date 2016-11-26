40a13073609b7022638e6461b16f58fb0dd869d4
Kittus <markasenjo@hotmail.com>
2016-11-26 00:09:16 +0000

    -.-

--- proves.py
+++ proves.py
@@ -7,12 +7,14 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def hello_func():
-    print ('ohayo')
-	#return render_template('hello.html')
+    #print ('ohayo')
+    return render_template('hello.html')
 
 @socketio.on('my event')
 def handle_my_custom_event(json):
     print('received json: ' + str(json))
+    #emit('lul', "I am groot")
+    emit('my response', json)
 
 if __name__ == '__main__':
-    socketio.run(app, '0.0.0.0', port=5000)
+    socketio.run(app)
