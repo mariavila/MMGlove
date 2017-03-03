6173ecaa4c951300a485cf42c302c22e335bb36b
Kittus <markasenjo@hotmail.com>
2017-03-03 13:23:58 +0100

    ;

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 #Trying if it works
 
 while 1:
-    pibrella.light.red.on();
-    time.sleep(3); # 3secs
-    pibrella.light.red.off();
-    time.sleep(3); # 3secs
+    pibrella.light.red.on()
+    time.sleep(3) # 3secs
+    pibrella.light.red.off()
+    time.sleep(3) # 3secs
