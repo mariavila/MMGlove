c0962d28412e3a3d168b6532aaf2d66ecbe6ecf5
mariavila <mariava.1213@gmail.com>
2017-03-10 13:14:51 +0100

    anem a fer proves!

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import time
 #Trying if it works
 
 while 1:
+    explorerhat.light.red.off()
+    time.sleep(3) # 3secs
+    explorerhat.light.green.on()
+    time.sleep(3) # 3secs
+    explorerhat.light.green.off()
     if(explorerhat.input.one.read()==1):
         explorerhat.light.red.on()
     else:
-        explorerhat.light.red.off()
-    explorerhat.light.green.on();
-    time.sleep(3) # 3secs
+        explorerhat.light.red.toggle()
