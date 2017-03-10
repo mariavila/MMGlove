882ada4e649944d172f687dd74dcd7566c9d1761
mariavila <mariava.1213@gmail.com>
2017-03-10 12:32:31 +0100

    anem a fer proves!

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import time
 #Trying if it works
 
 while 1:
-    if(pirbella.input.e.read()==1):
+    if(pibrella.input.e.read()==1):
         pibrella.light.red.on()
     else:
         pibrella.light.red.off()
-    pirbella.light.blue.on();
+    pibrella.light.green.on();
     time.sleep(3) # 3secs
