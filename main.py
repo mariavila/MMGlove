b2697be983c12f12c40ec69b0b234f416fd9fed3
mariavila <mariava.1213@gmail.com>
2017-03-17 13:27:36 +0100

    playing background sounds v5

--- main.py
+++ main.py
@@ -8,14 +8,24 @@ pygame.init()
 
 quin=0;
 while 1:
-    #quin=1
     if(explorerhat.input.one.read()==1):
-        if quin!=1:
-            pygame.mixer.music.load("mario.mp3")
-            pygame.mixer.music.play()
-            quin=1
+        if(explorerhat.input.two.read()==1):
+            if(quin !=5):
+                pygame.mixer.music.load("/Dit12/*.mp3")
+                pygame.mixer.music.play()
+                quin=5
+        else:
+            if quin!=1:
+                pygame.mixer.music.load("/Dit1/*.mp3")
+                pygame.mixer.music.play()
+                quin=1
     else:
-        if(quin!=0):
+        if(explorerhat.input.two.read()==1):
+            if(quin!=2):
+                pygame.mixer.music.load("/Dit2/*.mp3")
+                pygame.mixer.music.play()
+                quin=2
+        else:
             pygame.mixer.music.stop()
-            quin=0
+            quin=2
     time.sleep(0.2) # 3secs
