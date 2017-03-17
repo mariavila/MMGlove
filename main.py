31213af702040be0eaec29f5ee91f024b997de75
mariavila <mariava.1213@gmail.com>
2017-03-17 13:40:14 +0100

    playing background sounds v7

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ pygame.init()
 quin=0;
 while 1:
     if(explorerhat.input.one.read()==1):
+        print("entro 1")
         if(explorerhat.input.two.read()==1):
+            print("entro 12")
             if(quin !=5):
                 pygame.mixer.music.load("/Dit12/tetris.mp3")
                 pygame.mixer.music.play()
@@ -21,6 +23,7 @@ while 1:
                 quin=1
     else:
         if(explorerhat.input.two.read()==1):
+            print("entro 2")
             if(quin!=2):
                 pygame.mixer.music.load("/Dit2/zelda.mp3")
                 pygame.mixer.music.play()
