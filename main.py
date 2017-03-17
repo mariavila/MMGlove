973c14895fe0778eee86694699da637f770c28fe
mariavila <mariava.1213@gmail.com>
2017-03-17 13:08:27 +0100

    playing background sounds v3

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import pygame
 #Trying if it works
 pygame.mixer.init()
 pygame.init()
-s=pygame.mixer.music.load("mario.mp3")
+pygame.mixer.music.load("mario.mp3")
 while 1:
     #explorerhat.light.blue.off()
     #time.sleep(2) # 3secs
 
     if(explorerhat.input.one.read()==1):
-        s.play()
+        pygame.mixer.music.play()
         #explorerhat.light.blue.on()
     else:
-        s.stop()
+        pygame.mixer.music.stop()
         #pygame.mixer.stop()
         explorerhat.light.blue.off()
     time.sleep(0.2) # 3secs
