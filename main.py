ae406b8d63b87b32b28973ac2ed6ef2fd4f849e1
mariavila <mariava.1213@gmail.com>
2017-03-17 13:59:10 +0100

    debugging

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ quin=0;
 while 1:
     if(explorerhat.input.one.read()==1):
         if(explorerhat.input.two.read()==1):
-            print("entro 12")
             if(quin !=5):
+                print("entro 12")
                 pygame.mixer.music.load("Dit12/tetris.mp3")
                 pygame.mixer.music.play()
                 quin=5
         else:
-            print("entro 1")
             if quin!=1:
+                print("entro 1")
                 pygame.mixer.music.load("Dit1/mario.mp3")
                 pygame.mixer.music.play()
                 quin=1
     else:
         if(explorerhat.input.two.read()==1):
-            print("entro 2")
             if(quin!=2):
+                print("entro 2")
                 pygame.mixer.music.load("Dit2/zelda.mp3")
                 pygame.mixer.music.play()
                 quin=2
