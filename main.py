f781e4136e2f25591a29ea5dc8e52eb8a2f29bea
Kittus <markasenjo@hotmail.com>
2017-05-26 12:57:50 +0200

    debugging, ++mode

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ thread = threading.Thread(None, runFunc)
 
 @app.route('/')
 def hello_func():
-    #print ('ohayo')
     return render_template('hello.html')
 
 @socketio.on('my event')
@@ -24,7 +23,6 @@ def handle_my_custom_event(json):
     socketio.emit('ping event', {'data': 42})
 
 pygame.init()
-#pygame.mixer.init()
 pygame.midi.init()
 
 player = pygame.midi.Output(2)
@@ -32,6 +30,12 @@ player.set_instrument(0)
 
 playingTune = 0
 caseVar = 0
+
+#mode == 0 : Welcome screen
+#mode == 1 : Instrument selection
+#mode == 2 : Recording
+#mode == 3 : Pause Menu
+#mode == 4 : Listening
 mode = 0
 
 if __name__ == '__main__':
@@ -42,72 +46,70 @@ if __name__ == '__main__':
 
         if playingTune != caseVar:
             #if (caseVar == 0):
-                #pygame.mixer.music.stop()
-
-
             if (caseVar == 1):
                 print("entro 1")
                 socketio.emit('finger1')
-                #pygame.mixer.music.load("Dit1/mario.mp3")
-                #pygame.mixer.music.play()
+
                 if mode==0:
+                    mode+=1
+                if mode==1:
                     player.set_instrument(0)
                     mode+=1
                     playingTune=20
-		    notes=[]
-                elif mode==1:
+		            notes=[]
+                elif mode==2:
                     mode+=1
                     playingTune=20
-                elif mode==2:
+                elif mode==3:
                     file= open("music.txt", 'w')
                     ini=0
                     for aux in notes:
                         if aux==48:
                             if ini==0:
                                 file.write("DO")
-				ini=1
+				                ini=1
                             else:
                                 file.write(", DO")
                         elif aux==50:
                             if ini==0:
                                 file.write("RE")
-				ini=1
+				                ini=1
                             else:
                                 file.write(", RE")
                         elif aux==52:
                             if ini==0:
                                 file.write("MI")
-				ini=1
+				                ini=1
                             else:
                                 file.write(", MI")
                         elif aux==53:
                             if ini==0:
                                 file.write("FA")
-				ini=1
+				                ini=1
                             else:
                                 file.write(", FA")
                         elif aux==55:
                             if ini==0:
                                 file.write("SOL")
-				ini=1
+				                ini=1
                             else:
                                 file.write(", SOL")
                         elif aux==57:
                             if ini==0:
                                 file.write("LA")
-				ini=1
+				                ini=1
                             else:
                                 file.write(", LA")
                         elif aux==59:
                             if ini==0:
                                 file.write("SI")
-				ini=1
+				                ini=1
                             else:
                                 file.write(", SI")
                         elif aux==60:
                             if ini==0:
                                 file.write("DO4")
-				ini=1
+				                ini=1
                             else:
                                 file.write(", DO4")
                     file.close()
@@ -116,26 +118,25 @@ if __name__ == '__main__':
 
             elif (caseVar == 2):
                 print("entro 2")
-                #pygame.mixer.music.load("Dit2/zelda.mp3")
-                #pygame.mixer.music.play()
-                if mode==0:
+
+                if mode==1:
                     socketio.emit('finger2')
                     player.set_instrument(24)
                     mode+=1
                     playingTune=20
-		    notes=[]
-                elif mode==1:
+		            notes=[]
+                elif mode==2:
                     player.note_on(48, 127)
                     time.sleep(0.5)
                     player.note_off(48, 127)
                     notes= notes + [48]
-                elif mode==2:
+                elif mode==3:
                     socketio.emit('finger2')
                     notes=notes[:-1]
 
             elif (caseVar == 3):
                 print("entro 1+2")
-                if mode ==1:
+                if mode ==2:
                     player.note_on(53, 127)
                     time.sleep(0.5)
                     player.note_off(53, 127)
@@ -143,18 +144,18 @@ if __name__ == '__main__':
 
             elif (caseVar == 4):
                 print("entro 3")
-                if mode==0:
+                if mode==1:
                     socketio.emit('finger3')
                     player.set_instrument(40)
                     mode+=1
                     playingTune=20
-		    notes=[]
-                elif mode ==1:
+		            notes=[]
+                elif mode ==2:
                     player.note_on(50, 127)
                     time.sleep(0.5)
                     player.note_off(50, 127)
                     notes= notes + [50]
-                elif mode==2:
+                elif mode==3:
                     socketio.emit('finger3')
                     aux=0
                     for aux in notes:
@@ -164,7 +165,7 @@ if __name__ == '__main__':
 
             elif (caseVar == 5):
                 print("entro 1+3")
-                if mode ==1:
+                if mode ==2:
                     player.note_on(55, 127)
                     time.sleep(0.5)
                     player.note_off(55, 127)
@@ -172,7 +173,7 @@ if __name__ == '__main__':
 
             elif (caseVar == 6):
                 print("entro 2+3")
-                if mode ==1:
+                if mode ==2:
                     player.note_on(59, 127)
                     time.sleep(0.5)
                     player.note_off(59, 127)
@@ -183,27 +184,25 @@ if __name__ == '__main__':
 
             elif (caseVar == 8):
                 print("entro 4")
-                if mode==0:
+                if mode==1:
                     socketio.emit('finger4')
                     player.set_instrument(65)
                     mode+=1
                     playingTune=20
-		    notes=[]
-                elif mode ==1:
+		            notes=[]
+                elif mode ==2:
                     player.note_on(52, 127)
                     time.sleep(0.5)
                     player.note_off(52, 127)
                     notes= notes + [52]
-                elif mode==2:
+                elif mode==3:
                     socketio.emit('finger4')
                     mode = 1
                     playingTune=20
-                #pygame.mixer.music.load("Dit4/pokemon.mp3")
-                #pygame.mixer.music.play()
 
             elif (caseVar == 9):
                 print("entro 1+4")
-                if mode ==1:
+                if mode ==2:
                     player.note_on(57, 127)
                     time.sleep(0.5)
                     player.note_off(57, 127)
@@ -217,7 +216,7 @@ if __name__ == '__main__':
 
             elif (caseVar == 12):
                 print("entro 3+4")
-                if mode ==1:
+                if mode ==2:
                     player.note_on(60, 127)
                     time.sleep(0.5)
                     player.note_off(60, 127)
