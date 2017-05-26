de32b3c984eb125dd015140a14a5bed0d39bfae9
Kittus <markasenjo@hotmail.com>
2017-05-26 13:59:52 +0200

    nanae, nanana, nanunana

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def hello_func():
 @socketio.on('my event')
 def handle_my_custom_event(json):
     print('received json: ' + str(json))
-    socketio.emit('ping event', {'data': 42})
+    #socketio.emit('ping event', {'data': 42})
 
 pygame.init()
 pygame.midi.init()
@@ -45,190 +45,193 @@ if __name__ == '__main__':
         caseVar = explorerhat.input.one.read() + explorerhat.input.two.read() * 2 + explorerhat.input.three.read() * 4 + explorerhat.input.four.read() * 8
 
         if playingTune != caseVar:
-            #if (caseVar == 0):
-            if (caseVar == 1):
-                print("entro 1")
-                socketio.emit('finger1')
+            time.sleep(0.1) # 0.1secs
+            caseVar2 = explorerhat.input.one.read() + explorerhat.input.two.read() * 2 + explorerhat.input.three.read() * 4 + explorerhat.input.four.read() * 8
 
-                if mode==0:
-                    mode+=1
-                elif mode==1:
-                    player.set_instrument(0)
-                    mode+=1
-                    playingTune=20
-	            notes=[]
-                elif mode==2:
-                    mode+=1
-                    playingTune=20
-                elif mode==3:
-                    file= open("music.txt", 'w')
-                    ini=0
-                    for aux in notes:
-                        if aux==48:
-                            if ini==0:
-                                file.write("DO")
-		                ini=1
-                            else:
-                                file.write(", DO")
-                        elif aux==50:
-                            if ini==0:
-                                file.write("RE")
-		                ini=1
-                            else:
-                                file.write(", RE")
-                        elif aux==52:
-                            if ini==0:
-                                file.write("MI")
-		                ini=1
-                            else:
-                                file.write(", MI")
-                        elif aux==53:
-                            if ini==0:
-                                file.write("FA")
-		                ini=1
-                            else:
-                                file.write(", FA")
-                        elif aux==55:
-                            if ini==0:
-                                file.write("SOL")
-		                ini=1
-                            else:
-                                file.write(", SOL")
-                        elif aux==57:
-                            if ini==0:
-                                file.write("LA")
-		                ini=1
-                            else:
-                                file.write(", LA")
-                        elif aux==59:
-                            if ini==0:
-                                file.write("SI")
-		                ini=1
-                            else:
-                                file.write(", SI")
-                        elif aux==60:
-                            if ini==0:
-                                file.write("DO4")
-		                ini=1
-                            else:
-                                file.write(", DO4")
-                    file.close()
-                    mode = 0
-                    playingTune=20
+            if caseVar == caseVar2:
+                if (caseVar == 1):
+                    print("entro 1")
+                    socketio.emit('finger1')
 
-            elif (caseVar == 2):
-                print("entro 2")
+                    if mode==0:
+                        mode+=1
+                    elif mode==1:
+                        player.set_instrument(65)
+                        mode+=1
+                        playingTune=20
+    	            notes=[]
+                    elif mode==2:
+                        mode+=1
+                        playingTune=20
+                    elif mode==3:
+                        file= open("music.txt", 'w')
+                        ini=0
+                        for aux in notes:
+                            if aux==48:
+                                if ini==0:
+                                    file.write("DO")
+    		                ini=1
+                                else:
+                                    file.write(", DO")
+                            elif aux==50:
+                                if ini==0:
+                                    file.write("RE")
+    		                ini=1
+                                else:
+                                    file.write(", RE")
+                            elif aux==52:
+                                if ini==0:
+                                    file.write("MI")
+    		                ini=1
+                                else:
+                                    file.write(", MI")
+                            elif aux==53:
+                                if ini==0:
+                                    file.write("FA")
+    		                ini=1
+                                else:
+                                    file.write(", FA")
+                            elif aux==55:
+                                if ini==0:
+                                    file.write("SOL")
+    		                ini=1
+                                else:
+                                    file.write(", SOL")
+                            elif aux==57:
+                                if ini==0:
+                                    file.write("LA")
+    		                ini=1
+                                else:
+                                    file.write(", LA")
+                            elif aux==59:
+                                if ini==0:
+                                    file.write("SI")
+    		                ini=1
+                                else:
+                                    file.write(", SI")
+                            elif aux==60:
+                                if ini==0:
+                                    file.write("DO4")
+    		                ini=1
+                                else:
+                                    file.write(", DO4")
+                        file.close()
+                        mode = 0
+                        playingTune=20
 
-                if mode==1:
-                    socketio.emit('finger2')
-                    player.set_instrument(24)
-                    mode+=1
-                    playingTune=20
-	            notes=[]
-                elif mode==2:
-                    player.note_on(48, 127)
-                    time.sleep(0.5)
-                    player.note_off(48, 127)
-                    notes= notes + [48]
-                elif mode==3:
-                    socketio.emit('finger2')
-                    notes=notes[:-1]
+                elif (caseVar == 2):
+                    print("entro 2")
 
-            elif (caseVar == 3):
-                print("entro 1+2")
-                if mode ==2:
-                    player.note_on(53, 127)
-                    time.sleep(0.5)
-                    player.note_off(53, 127)
-                    notes= notes + [53]
-
-            elif (caseVar == 4):
-                print("entro 3")
-                if mode==1:
-                    socketio.emit('finger3')
-                    player.set_instrument(40)
-                    mode+=1
-                    playingTune=20
-	            notes=[]
-                elif mode ==2:
-                    player.note_on(50, 127)
-                    time.sleep(0.5)
-                    player.note_off(50, 127)
-                    notes= notes + [50]
-                elif mode==3:
-                    socketio.emit('finger3')
-                    aux=0
-                    for aux in notes:
-                        player.note_on(aux, 127)
+                    if mode==1:
+                        socketio.emit('finger2')
+                        player.set_instrument(40)
+                        mode+=1
+                        playingTune=20
+    	            notes=[]
+                    elif mode==2:
+                        player.note_on(48, 127)
                         time.sleep(0.5)
-                        player.note_off(aux, 127)
+                        player.note_off(48, 127)
+                        notes= notes + [48]
+                    elif mode==3:
+                        socketio.emit('finger2')
+                        notes=notes[:-1]
 
-            elif (caseVar == 5):
-                print("entro 1+3")
-                if mode ==2:
-                    player.note_on(55, 127)
-                    time.sleep(0.5)
-                    player.note_off(55, 127)
-                    notes= notes + [55]
+                elif (caseVar == 3):
+                    print("entro 1+2")
+                    if mode ==2:
+                        player.note_on(53, 127)
+                        time.sleep(0.5)
+                        player.note_off(53, 127)
+                        notes= notes + [53]
 
-            elif (caseVar == 6):
-                print("entro 2+3")
-                if mode ==2:
-                    player.note_on(59, 127)
-                    time.sleep(0.5)
-                    player.note_off(59, 127)
-                    notes= notes + [59]
+                elif (caseVar == 4):
+                    print("entro 3")
+                    if mode==1:
+                        socketio.emit('finger3')
+                        player.set_instrument(24)
+                        mode+=1
+                        playingTune=20
+    	            notes=[]
+                    elif mode ==2:
+                        player.note_on(50, 127)
+                        time.sleep(0.5)
+                        player.note_off(50, 127)
+                        notes= notes + [50]
+                    elif mode==3:
+                        socketio.emit('finger3')
+                        aux=0
+                        for aux in notes:
+                            player.note_on(aux, 127)
+                            time.sleep(0.5)
+                            player.note_off(aux, 127)
 
-            elif (caseVar == 7):
-                print("entro 1+2+3")
+                elif (caseVar == 5):
+                    print("entro 1+3")
+                    if mode ==2:
+                        player.note_on(55, 127)
+                        time.sleep(0.5)
+                        player.note_off(55, 127)
+                        notes= notes + [55]
 
-            elif (caseVar == 8):
-                print("entro 4")
-                if mode==1:
-                    socketio.emit('finger4')
-                    player.set_instrument(65)
-                    mode+=1
-                    playingTune=20
-	            notes=[]
-                elif mode ==2:
-                    player.note_on(52, 127)
-                    time.sleep(0.5)
-                    player.note_off(52, 127)
-                    notes= notes + [52]
-                elif mode==3:
-                    socketio.emit('finger4')
-                    mode = 1
-                    playingTune=20
+                elif (caseVar == 6):
+                    print("entro 2+3")
+                    if mode ==2:
+                        player.note_on(59, 127)
+                        time.sleep(0.5)
+                        player.note_off(59, 127)
+                        notes= notes + [59]
 
-            elif (caseVar == 9):
-                print("entro 1+4")
-                if mode ==2:
-                    player.note_on(57, 127)
-                    time.sleep(0.5)
-                    player.note_off(57, 127)
-                    notes= notes + [57]
+                elif (caseVar == 7):
+                    print("entro 1+2+3")
 
-            elif (caseVar == 10):
-                print("entro 2+4")
+                elif (caseVar == 8):
+                    print("entro 4")
+                    if mode==1:
+                        socketio.emit('finger4')
+                        player.set_instrument(0)
+                        mode+=1
+                        playingTune=20
+    	            notes=[]
+                    elif mode ==2:
+                        player.note_on(52, 127)
+                        time.sleep(0.5)
+                        player.note_off(52, 127)
+                        notes= notes + [52]
+                    elif mode==3:
+                        socketio.emit('finger4')
+                        mode = 1
+                        playingTune=20
 
-            elif (caseVar == 11):
-                print("entro 1+2+4")
+                elif (caseVar == 9):
+                    print("entro 1+4")
+                    if mode ==2:
+                        player.note_on(57, 127)
+                        time.sleep(0.5)
+                        player.note_off(57, 127)
+                        notes= notes + [57]
 
-            elif (caseVar == 12):
-                print("entro 3+4")
-                if mode ==2:
-                    player.note_on(60, 127)
-                    time.sleep(0.5)
-                    player.note_off(60, 127)
-                    notes= notes + [60]
+                elif (caseVar == 10):
+                    print("entro 2+4")
 
-            elif (caseVar == 13):
-                print("entro 1+3+4")
+                elif (caseVar == 11):
+                    print("entro 1+2+4")
 
-            elif (caseVar == 14):
-                print("entro 2+3+4")
+                elif (caseVar == 12):
+                    print("entro 3+4")
+                    if mode ==2:
+                        player.note_on(60, 127)
+                        time.sleep(0.5)
+                        player.note_off(60, 127)
+                        notes= notes + [60]
 
-            elif (caseVar == 15):
-                print("entro 1+2+3+4")
+                elif (caseVar == 13):
+                    print("entro 1+3+4")
+
+                elif (caseVar == 14):
+                    print("entro 2+3+4")
+
+                elif (caseVar == 15):
+                    print("entro 1+2+3+4")
 
         time.sleep(0.05) # 0.05secs
